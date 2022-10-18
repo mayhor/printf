62db316b81d961160685a105d54fd8789a1c1a1f
Awodire Mayowa <mawodire@gmail.com>
2022-10-18 23:10:22 +0100

    Add: octal.c

--- octal.c
+++ octal.c
@@ -5,12 +5,12 @@
  * Return: number of characters
  */
 
-int print_o(va_list list)
+int print_o(va_list ar_list)
 {
 	char *o_buffer;
 	int size;
 
-	o_buffer = convert(va_arg(list, unsigned int), 8);
+	o_buffer = convert(va_arg(ar_list, unsigned int), 8);
 
 	size = print((o_buffer != NULL) ? o_buffer : "NULL");
 	return (size);
