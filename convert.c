d3c3491ef607a7bf19f1b1cefb8b378209d4c92e
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-18 14:46:16 -0700

    Add: convert notation

---
+++ convert.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * convert - convert num parameter to base
+ * @num: num argument
+ * @base: base number
+ * Return: pointer to character
+ */
+
+char *convert(long int num, int base)
+{
+	static char *array = "0123456789abcdef";
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
