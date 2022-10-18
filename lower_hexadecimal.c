3b126c81aa0ddfb29d00fba33a50d3133ce99ee4
Awodire Mayowa <mawodire@gmail.com>
2022-10-18 23:06:26 +0100

    Add: lower_hexadecimal.c

--- lower_hexadecimal.c
+++ lower_hexadecimal.c
@@ -5,12 +5,12 @@
  * Return: size
  */
 
-int print_x(va_list list)
+int print_x(va_list ar_list)
 {
 	char *x_buffer;
 	int size;
 
-	x_buffer = convert(va_arg(list, unsigned int), 16);
+	x_buffer = convert(va_arg(ar_list, unsigned int), 16);
 	size = print((x_buffer != NULL) ? x_buffer : "NULL");
 
 	return (size);
