18ada6a19f11bb32104c8588fdd159999c2b0307
Awodire Mayowa <mawodire@gmail.com>
2022-10-18 23:34:46 +0100

    Add: character_operations.c

--- character_operations.c
+++ character_operations.c
@@ -15,10 +15,10 @@ int print_c(va_list ar_list)
 }
 
 /**
-* print_s - print string (%s)
-* @ar_list: Store the a list of characters
-* Return: num of parameters printed
-*/
+ * print_s - print string (%s)
+ * @ar_list: Store the a list of characters
+ * Return: num of parameters printed
+ */
 
 
 int print_s(va_list ar_list)
